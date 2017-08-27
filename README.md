aa0795ba86d5d68f62d29d95b9cbc143ca03a0a5
Vitor Oliveitra <vitorcool@gmail.com>
2017-08-27 13:51:12 +0100

    ver 1.0

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # toaster-retry
-Easy to use, RETRY toastr UI. Retry notification between fail operation
+- Easy to use, RETRY UI.
+- Extension of toastr.js notifications. One notification type "retry", for error handling
+
+
+### Requirements:
+- [jquery/jquery] - New Wave JavaScript
+- [twbs/bootstrap] - Powerful front-end framework for faster and easier web development
+- [CodeSeven/toastr] - toastr is a Javascript library for non-blocking notifications.
+- [yesmeck/jquery-jsonview] - Formats & syntax highlights JSON.
+        
+### Setup
+* Simple (required javascript libraries will be auto-load). Note: In order to stylesheet defs work well, make sure toastr-retry.css is loaded after body tag        
+```
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">          
+            <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+            <script src="js/toastr-retry.js"></script>
+        </head>
+        
+        <body>
+            <link href="css/toastr-retry.css" rel="stylesheet"></link>
+```
+           
+        
+* Manual (load all javascript libraries)
+```HTML
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+                        <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
+                        <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery-jsonview/1.2.3/jquery.jsonview.css" rel="stylesheet">
+            
+            <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>      
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+            <script src="js/toastr-retry.js"></script>                 
+        </head>
+        
+        <body>
+            <link href="css/toastr-retry.css" rel="stylesheet"></link>
+```
+
+* Current version
+        
+        
+[jquery/jquery]: <https://github.com/jquery/jquery>
+[twbs/bootstrap]: <https://github.com/twbs/bootstrap>
+[CodeSeven/toastr]: <https://github.com/CodeSeven/toastr>
+[yesmeck/jquery-jsonview]: <https://github.com/yesmeck/jquery-jsonview>
+
+
--- js/toastr-retry.js
+++ js/toastr-retry.js
@@ -1,12 +1,9 @@
 /**
  * Created by vitorcool on 8/21/2017.
  */
-window.addEventListener("resize",function(event){
-    if(typeof jQuery!=="undefined") {
-        $("#toastr-container").css('maxWidth')
-    }
-})
-document.addEventListener("DOMContentLoaded", function(event) {
+
+$.holdReady(true);
+window.addEventListener('DOMContentLoaded', function(event) {
     var loadJS = function(isLoaded, url, implementationCode, location){
         location=typeof(location)=="undefined" ? document.body : location;
         if(isLoaded) {
@@ -35,7 +32,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
     };
 
     // load jquery
-    loadJS(typeof jQuery !== "undefined",               "http://code.jquery.com/jquery-latest.min.js",function(){
+    if(typeof jQuery == "undefined"){
+        console.log('toastr-retry error. jQuery must be loaded for toastr-retry to function.')
+    }else{
         // load bootstrap
         loadCSS(typeof $().modal !== "undefined",       "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
         loadJS(typeof $().modal !== "undefined",        "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js");
@@ -52,8 +51,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
             $( window ).bind( "resize", function(){
                 toastr.setContainerHeight();
             });
-            toastr.setContainerHeight();
+            $( document.body ).bind("DOMSubtreeModified", function(event) {
+                if (event.target!==document.body)
+                    return;
+                if($(document.body).children("#toast-container").length==1) {
+                    toastr.setContainerHeight()
+                }
+            });
 
+            // JSON propperties also tooogle (expand,collapse) children elements
             $(document).on('click','.jsonview .prop',function(event){
                 //event.stopPropagation();
                 var e=$(event.target).prev(".collapser");
@@ -64,29 +70,105 @@ document.addEventListener("DOMContentLoaded", function(event) {
             toastr.retry=function(toPush, responseObj, options ) {
                 return new retryToaster(toPush,responseObj,options);
             }
+
+            // handle click and drag
+            var mousePos, scrollPos, curDown=false;
+            var getSelectionText = function() {
+                var text = "";
+                if (window.getSelection) {
+                    text = window.getSelection().toString();
+                } else if (document.selection && document.selection.type != "Control") {
+                    text = document.selection.createRange().text;
+                }
+                return text;
+            }
+            $(document.body).on('mousemove','#toast-container', function(e){
+                if(curDown){
+                    var scroller = $('#toast-container');
+                    var max = {
+                        y: scroller[0].scrollHeight - scroller.outerHeight(),
+                        x: scroller[0].scrollWidth  - scroller.outerWidth(),
+                    }
+                    var scrollTo = {
+                        y : Math.min( scrollPos.y + (mousePos.y - e.pageY), max.y),
+                        x : Math.min( scrollPos.x + (mousePos.x - e.pageX), max.x)
+                    }
+                    var scrollTo = {
+                        y : Math.max( 0, scrollTo.y ),
+                        x : Math.max( 0, scrollTo.x )
+                    }
+                    $('html').css('cursor', 'row-resize');
+                    scroller.scrollTop( scrollTo.y );
+                    scroller.scrollLeft(scrollTo.x );
+                    console.log({
+                            x: scrollTo.x,
+                            y: scrollTo.y
+                        });
+                }
+            });
+
+            var intervalClickDownClickUp=null;
+            $(document.body).on('mousedown','#toast-container', function(e){
+                mousePos = {y:e.pageY,x:e.pageX};
+                var scroller = $('#toast-container');
+                scrollPos = {y:scroller.scrollTop(),x:scroller.scrollLeft()};
+                intervalClickDownClickUp = window.setTimeout(function() {
+                    if (getSelectionText() == ""){
+                        scroller.css({
+                            'MozUserSelect': 'none',
+                            'webkitUserSelect': 'none'
+                        }).attr('unselectable', 'on').bind('selectstart', function () {
+                            return false;
+                        });
+                        curDown = true;
+                    }
+                    intervalClickDownClickUp=null;
+                },250);
+
+            });
+
+            $(document.body).on('mouseup','#toast-container', function(e){
+                if(intervalClickDownClickUp!=null){
+                    window.clearTimeout(intervalClickDownClickUp);
+                    intervalClickDownClickUp=null;
+                }
+                curDown = false;
+                var scroller = $('#toast-container');
+                scroller.css({
+                    'MozUserSelect':'',
+                    'webkitUserSelect':''
+                }).attr('unselectable','off').unbind('selectstart');
+                $('html').css('cursor', 'auto');
+            });
+
+            $.holdReady(false);
         });
         // load JSONView
         loadCSS(typeof $.fn.JSONView !== "undefined", "https://cdnjs.cloudflare.com/ajax/libs/jquery-jsonview/1.2.3/jquery.jsonview.css");
         loadJS(typeof $.fn.JSONView !== "undefined",  "https://cdnjs.cloudflare.com/ajax/libs/jquery-jsonview/1.2.3/jquery.jsonview.js");
-    });
+    }
 
 });
 
 toasterRetryContainerId=1;
 var retryToaster = function(to222Push,responseObj, options ){
-    //if( $("#retryToaster-"+(window.toasterRetryContainerId)).length==0 ) return;
-    window.toasterRetryContainerId++;
+    var idx=window.toasterRetryContainerId++;
+    var me=this;
+
     Const = retryButtonClass = 'toast-retryButton';
     Const = toggleInfoButtonClass = 'toast-toggleInfoButton';
     Const = toasterRetryClass= 'toast-retry';
     Const = toasterJsonClass = 'toast-json';
 
+    this.consoleLog = false;
     this.retryButtonText = "RETRY",
+
     this.closeButton = true,
-    this.timeOut = 0,
+    this.timeOut = 10000,
     this.tapToDismiss = false,
     this.closeOnHover = false,
-    this.extendedTimeOut = 0,
+    this.extendedTimeOut = 2000,
+
     this.showInfo = true,
     this.pushAfterClose = false,
     this.onText = null,
@@ -96,11 +178,33 @@ var retryToaster = function(to222Push,responseObj, options ){
     this.onCancel=null,
     this.onRetry = null;
 
-    var me=this;
+    // set options
+    this.setOptions=function(options) {
+        for (var key in options) {
+            if (typeof(me[key]) !== 'undefined') {
+                me[key] = options[key];
+            }
+        }
+        var setToast=function(prop,val){
+            if(typeof(val)!='undefined' && toasterOptions[prop]!=val)
+                toasterOptions[prop]=val;
+        }
+        setToast('closeButton'  ,options.closeButton);
+        setToast('tapToDismiss' ,options.tapToDismiss);
+        setToast('closeOnHover' ,options.closeOnHover);
+        setToast('extendedTimeOut',options.extendedTimeOut);
+
+    },
+    this.getOptions=function(options) {
+        return options;
+    },
+    this.getToasterOptions=function() {
+        return toasterOptions;
+    };
 
     var toasterOptions={
         closeButton     :   this.closeButton,
-        timeOut         :   this.timeOut,
+        timeOut         :   0,
         tapToDismiss    :   this.tapToDismiss,
         closeOnHover    :   this.closeOnHover,
         extendedTimeOut :   this.extendedTimeOut,
@@ -127,40 +231,20 @@ var retryToaster = function(to222Push,responseObj, options ){
         },
     };
 
-    // set options
-    this.setOptions=function(options) {
-        for (var key in options) {
-            if (typeof(me[key]) !== 'undefined') {
-                me[key] = options[key];
-            }
-        }
-        var setToast=function(prop,val){
-            if(typeof(val)!='undefined' && toasterOptions[prop]!=val)
-                toasterOptions[prop]=val;
-        }
-        setToast('closeButton'  ,options.closeButton);
-        setToast('timeOut'      ,options.timeOut);
-        setToast('tapToDismiss' ,options.tapToDismiss);
-        setToast('closeOnHover' ,options.closeOnHover);
-        setToast('extendedTimeOut',options.extendedTimeOut);
-
-    },
-        this.getOptions=function(options) {
-            return options;
-        },
-        this.getToasterOptions=function() {
-            return toasterOptions;
-        }
-
-
     this.setOptions(options);
 
-    var doRetry = function(){
-            console.log('doRetry');
+    var
+        consoleLog=function(text) {
+            if(this.consoleLog)
+                console.log(text);
+        },
+
+        doRetry = function(){
+            consoleLog('doRetry');
             // force toaster down
             toastr.clear( thisToaster ,{force:true});
             if(typeof(me.onRetry)=='function')
-                console.log('onRetry');
+                consoleLog('onRetry');
             if(this.pushAfterClose)
                 window.setTimeout(function(){
                     me.onRetry.apply( me, [to222Push, responseObj,doCalcRemainingNonPushed()] );
@@ -170,16 +254,16 @@ var retryToaster = function(to222Push,responseObj, options ){
         },
 
         doCancel = function(){
-            console.log('doCancel');
+            consoleLog('doCancel');
             if(typeof(me.onCancel)==='function') {
-                console.log('onCancel');
+                consoleLog('onCancel');
                 me.onCancel.apply(me, [to222Push, responseObj,doCalcRemainingNonPushed()]);
             }
         },
         doJSON = function(){
-            console.log('doJSON');
+            consoleLog('doJSON');
             if(typeof(me.onJSON)==='function') {
-                console.log('onJSON');
+                consoleLog('onJSON');
                 return me.onJSON.apply(me, [to222Push, responseObj,doCalcRemainingNonPushed()]);
             }else if(typeof(me.onJSON)==='object' && me.onJSON!==null) {
                 return me.onJSON;
@@ -194,23 +278,23 @@ var retryToaster = function(to222Push,responseObj, options ){
         },
         remainingNonPushed=null,
         doCalcRemainingNonPushed=function(){
-            console.log('doCalcRemainingNonPushed');
+            consoleLog('doCalcRemainingNonPushed');
             if(remainingNonPushed!=null)
                 return remainingNonPushed;
             if(typeof(me.onCalcRemainingNonPushed)==='function') {
-                console.log('onCalcRemainingNonPushed');
+                consoleLog('onCalcRemainingNonPushed');
                 return me.onCalcRemainingNonPushed.apply(me,[to222Push, responseObj] );
             }else
                 return [];
         },
 
         doText=function(){
-            console.log('doText');
+            consoleLog('doText');
             var ret='';
             var r = doCalcRemainingNonPushed();
 
             if(typeof(me.onText)==='function') {
-                console.log('onText');
+                consoleLog('onText');
                 ret += me.onText.apply(me,[to222Push, responseObj,r] );
             }else if(typeof(me.onText)==='string') {
                 ret = me.onText;
@@ -221,16 +305,16 @@ var retryToaster = function(to222Push,responseObj, options ){
             ret += '<br><button type="button" class="'+retryButtonClass+' btn clear">'+me.retryButtonText+'</button>';
             if(me.showInfo) {
                 ret+= '<a href="#" aria-expanded="false" class="'+toggleInfoButtonClass+' btn clear glyphicon" '+
-                    'data-toggle="collapse" data-target="#json-'+window.toasterRetryContainerId+'"><span class="glyphicon"></span></a>' ;
-                ret+= '<br><pre id="json-'+window.toasterRetryContainerId+'" class="'+toasterJsonClass+' collapse"></pre>';
+                    'data-toggle="collapse" data-target="#json-'+idx+'"><span class="glyphicon"></span></a>' ;
+                ret+= '<br><pre id="json-'+idx+'" class="'+toasterJsonClass+' collapse"></pre>';
             }
             return ret;
         },
 
         doTitle=function(){
-            console.log('doTitle');
+            consoleLog('doTitle');
             if(typeof(me.onTitle)==='function') {
-                console.log('onTitle');
+                consoleLog('onTitle');
                 return me.onTitle.apply(me,[to222Push, responseObj,doCalcRemainingNonPushed()] );
             }else if(typeof(me.onTitle)==='string') {
                 return me.onTitle;
@@ -240,20 +324,46 @@ var retryToaster = function(to222Push,responseObj, options ){
         }
 
 
+
     remainingNonPushed = doCalcRemainingNonPushed();
 
     var thisToaster=toastr.error(doText(), doTitle(), toasterOptions);
     // set maxHeight 4 #toastr-container
     toastr.setContainerHeight();
     // create identity
-    $(thisToaster).prop('id', "retryToaster-"+(window.toasterRetryContainerId) );
+    $(thisToaster).prop('id', "retryToaster-"+(idx) );
     // share retry-funcs
     $(thisToaster).data('retry-funcs',this);
     // add instance CSS Class
     $(thisToaster).addClass(toasterRetryClass);
+    // handle JSON info
     if(me.showInfo) {
         $(thisToaster).find( '.'+toasterJsonClass ).JSONView(doJSON(), {collapsed: true});
         $(thisToaster).find( '.'+toasterJsonClass ).JSONView('toggle', 2)
     }
+    // handle timeOut
+    var interval4close=null;
+    if(me.timeOut>0){
+        var maxTime = parseFloat(new Date().getTime()) + me.timeOut;
+        var isMouseHouver = false;
+        $("#retryToaster-"+(idx)).mouseleave(function(){
+            consoleLog("MouseOut");
+            isMouseHouver = false;
+            maxTime = Math.max(maxTime, parseFloat(new Date().getTime()) + me.extendedTimeOut );
+        }).mouseenter(function(){
+            consoleLog("MouseOut");
+            isMouseHouver = true;
+            maxTime = Math.max(maxTime, parseFloat(new Date().getTime()) + me.extendedTimeOut );
+        })
+        interval4close=window.setInterval(function() {
+            if((!isMouseHouver && (new Date().getTime()>maxTime)) ) {
+                toastr.clear(thisToaster, {force: true});
+                window.clearInterval(interval4close);
+            }else if(!$(thisToaster).is(":visible")) {
+                window.clearInterval(interval4close);
+            }
+        },250);
+    }
+
 }
 
